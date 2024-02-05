c58d22f04aa65f532300b299772b4edff1beeb7f
hurmatayub <156796678+hurmatayub@users.noreply.github.com>
2024-02-05 21:08:05 +0500

    Add files via upload

---
+++ HurmatAyub.py
@@ -0,0 +1,28 @@
+
+##FOR LOOP
+
+number=int(input("Enrer any number:"))
+print("The multiplication Table of:",number)
+
+for digits in range(1, 11):
+   print(number, 'x', digits, '=', number*digits)
+
+
+## WHILE LOOP
+ 
+number=int(input("Enrer any number:"))
+count = 1
+print("The multiplication Table of:",number)
+while count<=10:
+    number = number*1
+    print(number, 'x', count, '=', number*count)
+    count+=1
+
+
+
+
+
+
+    
+
+
